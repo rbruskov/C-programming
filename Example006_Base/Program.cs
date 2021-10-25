daaf69e8bb1e81224ce1f7eab52d115e34011f5e
Ruslan <rbruskov@mail.ru>
2021-10-25 23:31:34 +0300

    a>max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = 1;
+int B = 2;
+int C = 6;
+int D = 3;
+
+int max = A;
+
+if(A > max )  max = A;
+if(B > max )  max = B;
+if(C > max )  max = C;
+if(D > max )  max = D;
+
+Console.Write("max =");
+Console.WriteLine(max);
