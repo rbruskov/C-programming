bc02c1bf2eb68e50f4cd0bcc5de66b4b8bc02cda
Ruslan <rbruskov@mail.ru>
2021-10-25 23:02:37 +0300

    ToLower()

---
+++ Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите ваше имя:");
+String username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+  Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+   Console.Write("Привет,"); 
+   Console.Write(username);
+}
