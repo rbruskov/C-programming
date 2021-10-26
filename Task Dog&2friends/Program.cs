2675d1b4b574ea6d988a207537562f75d04501e7
Ruslan <rbruskov@mail.ru>
2021-10-26 19:00:19 +0300

    dog and tw friends task

---
+++ Task Dog&2friends/Program.cs
@@ -0,0 +1,24 @@
+﻿double    count = 0;
+int    distance = 10000;
+int    friendspeed1=1;
+int    friendspeed2=2;
+int    dogspeed=5;
+int    friend=2;
+int    time = 0;
+
+    while(distance > 10)
+    {
+        if(friend ==1)
+        {
+            time=distance/(friendspeed1+dogspeed);
+            friend=2;
+        }
+        else 
+        {
+            time=distance/(friendspeed2+dogspeed);
+            friend=1;
+        }
+        distance=distance-(friendspeed1+friendspeed2)*time;
+        count++;
+    }
+Console.WriteLine(count);
